2a3b918f6f1f17052d0c2825242b842250932f7d
codingFish <wangliang199538@live.com>
2016-10-24 07:18:58 +0800

    读取excel内容并开始爬。

--- pys/beautfulsoup.py
+++ pys/beautfulsoup.py
@@ -1,6 +1,8 @@
 import sys
+import xlrd
 import requests
 from bs4 import BeautifulSoup
+
 
 reload(sys)
 sys.setdefaultencoding('utf8')
@@ -19,24 +21,47 @@ def print_key(text):
 
     # word
     span = soup.find('span', class_='keyword')
-    print span.get_text()
 
     # word's phonetic symbol
     span2 = soup.find('span', class_='phonetic')
-    print span2.get_text()
 
     # word's Chinese meaning
     div = soup.find('div', class_='trans-container')
     li = div.find('li')
-    print li.get_text()
 
+    a = span.get_text()
+    b = span2.get_text()
+    c = li.get_text()
+    print b
+    sql = "INSERT INTO words ( `word`, `phonetic_symbol`, `mean`) VALUES  ( '%s', '%s', '%s' );" % (a, b, c)
+    print sql
 
-words = ['apple', 'banana', 'teach', 'code', 'fire']
-
-for key in words:
+'''for key in words:
     url = 'http://www.youdao.com/w/eng/'
     html = load_html(url, key)
     print_key(html.text)
+'''
+
+
+fname = 'seed.xlsx'
+bk = xlrd.open_workbook(fname)
+
+try:
+    sh = bk.sheet_by_name('Sheet1')
+except:
+    print 'no sheet1'
+
+nrows = sh.nrows
+
+count = range(0, nrows+1)
+
+url = 'http://www.youdao.com/w/eng/'
+
+for index in count:
+    word = sh.cell_value(index, 0)
+    html = load_html(url, word)
+    print_key(html.text)
+
 
 
 
